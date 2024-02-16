bd04b1798c63e0037b27f3c522124736b3d67d5f
Micheline04 <michelineglenesia@gmail.com>
2024-02-16 14:42:37 +0700

    Perbaikan

--- backend/create.py
+++ backend/create.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # from flask import Flask
 # from webdata.models import db
 # from webdata.config import Config
@@ -14,21 +13,4 @@
     
 # if __name__ == '__main__':
 #     with app.app_context():
-=======
-# from flask import Flask
-# from webdata.models import db
-# from webdata.config import Config
-# from flask_sqlalchemy import SQLAlchemy
-
-# from webdata import app , db , bcrypt
-# from webdata.models import Pengguna, Ekspedisi, LimbahElektronik, Lokasi, Kupon, Transaksi_Penjemputan
-
-# def main():
-#     Pengguna1 = Pengguna(nama_pengguna='Lous', username='admin', email='lou@gmail.com' , nomor_telepon='08123456789', alamat='Jl. Kebon Jeruk No. 1', password=bcrypt.generate_password_hash('admin').decode('utf-8'))
-#     db.session.add(Pengguna1)
-#     db.session.commit()
-    
-# if __name__ == '__main__':
-#     with app.app_context():
->>>>>>> 5583e5c2ed37fa6a65c376e9592ebf0f9ecccac9
 #         main()
