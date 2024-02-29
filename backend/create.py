e13aade3201ba61cf80aeb7bca635c49d55fcd7b
Micheline04 <michelineglenesia@gmail.com>
2024-02-29 16:27:16 +0700

    Update backend dashboard

--- backend/create.py
+++ backend/create.py
@@ -5,10 +5,11 @@
 
 # from webdata import app , db , bcrypt
 # from webdata.models import Pengguna, Ekspedisi, LimbahElektronik, Lokasi, Kupon, Transaksi_Penjemputan
+# from datetime import datetime
 
 # def main():
-#     Pengguna1 = Pengguna(nama_pengguna='Lous', username='admin', email='lou@gmail.com' , nomor_telepon='08123456789', alamat='Jl. Kebon Jeruk No. 1', password=bcrypt.generate_password_hash('admin').decode('utf-8'))
-#     db.session.add(Pengguna1)
+#     Lokasi1 = Lokasi(id = 1 , nama_lokasi="Pengepul Barang Rongsokan Limbah Elektronik Barang Bekas Loak" , alamat_lokasi="Jl. Ledeng, RT.04/RW.03, Gunungbatu, Kec. Bogor Bar, Kota Bogor, Jawa Barat" )
+#     db.session.add(Lokasi1)
 #     db.session.commit()
     
 # if __name__ == '__main__':
