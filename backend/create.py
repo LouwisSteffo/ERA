80f493dc0925f1fe6ead81bd3c17232edac37144
LouwisSteffo <louwissteffo@gmail.com>
2024-03-14 12:16:56 +0700

    Push Louwis

--- backend/create.py
+++ backend/create.py
@@ -8,25 +8,35 @@ from webdata.models import Pengguna, Ekspedisi, LimbahElektronik, Lokasi, Kupon,
 from datetime import datetime
 
 def main():
-    Lokasi1 = Lokasi(id = 1 , nama_lokasi="Pengepul Barang Rongsokan Limbah Elektronik Barang Bekas Loak" , alamat_lokasi="Jl. Ledeng, RT.04/RW.03, Gunungbatu, Kec. Bogor Bar, Kota Bogor, Jawa Barat" )
-    Lokasi2 = Lokasi(id = 2 , nama_lokasi="Gudang Rongsokan Barang Bekas & Limbah Elektronik" , alamat_lokasi="Jl. Bogor Nirwana Residence, RT.01/RW.10, Ranggamekar, Kec. Bogor Sel., Kota Bogor, Jawa Barat" )
-    Lokasi3 = Lokasi(id = 3 , nama_lokasi="Gudang UD Jaya Abadi Scrapindo Pengepul Besi Tua Barang Bekas Loak & Limbah Elektronik Bogor" , alamat_lokasi="Jl. Kp. Sawah Kaum, RT.03 RW02/RW.09, Mekarjaya, Kec. Ciomas, Kabupaten Bogor, Jawa Barat 16610" )
-    Lokasi4 = Lokasi(id = 4 , nama_lokasi="Lapak Rongsok Barang Bekas & Limbah Elektronik Akar Makmur" , alamat_lokasi="Muara sari, RT.01/RW.12, Pasirkuda, Kec. Bogor Bar., Kota Bogor, Jawa Barat 16119" )
+    # Lokasi1 = Lokasi(id = 1 , nama_lokasi="Pengepul Barang Rongsokan Limbah Elektronik Barang Bekas Loak" , alamat_lokasi="Jl. Ledeng, RT.04/RW.03, Gunungbatu, Kec. Bogor Bar, Kota Bogor, Jawa Barat" )
+    # Lokasi2 = Lokasi(id = 2 , nama_lokasi="Gudang Rongsokan Barang Bekas & Limbah Elektronik" , alamat_lokasi="Jl. Bogor Nirwana Residence, RT.01/RW.10, Ranggamekar, Kec. Bogor Sel., Kota Bogor, Jawa Barat" )
+    # Lokasi3 = Lokasi(id = 3 , nama_lokasi="Gudang UD Jaya Abadi Scrapindo Pengepul Besi Tua Barang Bekas Loak & Limbah Elektronik Bogor" , alamat_lokasi="Jl. Kp. Sawah Kaum, RT.03 RW02/RW.09, Mekarjaya, Kec. Ciomas, Kabupaten Bogor, Jawa Barat 16610" )
+    # Lokasi4 = Lokasi(id = 4 , nama_lokasi="Lapak Rongsok Barang Bekas & Limbah Elektronik Akar Makmur" , alamat_lokasi="Muara sari, RT.01/RW.12, Pasirkuda, Kec. Bogor Bar., Kota Bogor, Jawa Barat 16119" )
     
-    Ekspedisi1 = Ekspedisi(id = 1, nama_ekspedisi = "JNE")
-    Ekspedisi2 = Ekspedisi(id = 2, nama_ekspedisi = "J&T Express")
-    Ekspedisi3 = Ekspedisi(id = 3, nama_ekspedisi = "GrabExpress")
-    Ekspedisi4 = Ekspedisi(id = 4, nama_ekspedisi = "Gosend")
+    # Ekspedisi1 = Ekspedisi(id = 1, nama_ekspedisi = "JNE")
+    # Ekspedisi2 = Ekspedisi(id = 2, nama_ekspedisi = "J&T Express")
+    # Ekspedisi3 = Ekspedisi(id = 3, nama_ekspedisi = "GrabExpress")
+    # Ekspedisi4 = Ekspedisi(id = 4, nama_ekspedisi = "Gosend")
     
-    db.session.add(Lokasi1)
-    db.session.add(Lokasi2)
-    db.session.add(Lokasi3)
-    db.session.add(Lokasi4)
+    # Kupon1 = Kupon(id = 1, nama_kupon = "Kupon Gopay", harga_kupon = 100 , gambar_kupon = "..", jumlah_kupon = 1500)
+    Kupon2 = Kupon(id = 2, nama_kupon = "Kupon Shopeepay", harga_kupon = 150 , gambar_kupon = "..", jumlah_kupon = 1500)
+    Kupon3 = Kupon(id = 3, nama_kupon = "Kupon OVO", harga_kupon = 100 , gambar_kupon = "..", jumlah_kupon = 1500)
+    Kupon4 = Kupon(id = 4, nama_kupon = "Kupon DANA", harga_kupon = 100 , gambar_kupon = "..", jumlah_kupon = 1500)
     
-    db.session.add(Ekspedisi1)
-    db.session.add(Ekspedisi2)
-    db.session.add(Ekspedisi3)
-    db.session.add(Ekspedisi4)
+    # db.session.add(Lokasi1)
+    # db.session.add(Lokasi2)
+    # db.session.add(Lokasi3)
+    # db.session.add(Lokasi4)
+    
+    # db.session.add(Ekspedisi1)
+    # db.session.add(Ekspedisi2)
+    # db.session.add(Ekspedisi3)
+    # db.session.add(Ekspedisi4)
+    
+    # db.session.add(Kupon1)
+    db.session.add(Kupon2)
+    db.session.add(Kupon3)
+    db.session.add(Kupon4)
     
     db.session.commit()
     
--- backend/webdata/dashboard/routes.py
+++ backend/webdata/dashboard/routes.py
@@ -6,30 +6,67 @@ from webdata import db , bcrypt
 
 dashboard = Blueprint('dashboard', __name__)
 
+
 @dashboard.route('/')
 @login_required
 def index():
+    count1 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Antrean').count()
+    count2 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Proses').count()
+    count3 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Selesai').count()
+    count4 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dibatalkan').count()
     list_transaksi = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Antrean').all()
     total = len(list_transaksi)
-    return render_template('dashboard/dashboard_dalam_antrean.html', list_transaksi=list_transaksi, total=total)
+    return render_template('dashboard/dashboard_dalam_antrean.html', list_transaksi=list_transaksi, count1=count1, count2=count2, count3=count3, count4=count4, total=total)
 
 @dashboard.route('/dalam_proses')
 @login_required
 def dalam_proses():
+    count1 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Antrean').count()
+    count2 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Proses').count()
+    count3 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Selesai').count()
+    count4 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dibatalkan').count()
     list_transaksi = Transaksi_Penjemputan.query.filter_by(status_transaksi = 'Dalam Proses').all()
     total = len(list_transaksi)
-    return render_template('dashboard/dashboard_dalam_proses.html', list_transaksi = list_transaksi, total=total)
+    return render_template('dashboard/dashboard_dalam_proses.html', list_transaksi = list_transaksi, total=total , count1=count1, count2=count2, count3=count3, count4=count4)
 
 @dashboard.route('/selesai')
 @login_required
 def selesai():
+    count1 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Antrean').count()
+    count2 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Proses').count()
+    count3 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Selesai').count()
+    count4 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dibatalkan').count()
     list_transaksi = Transaksi_Penjemputan.query.filter_by(status_transaksi = 'Selesai').all()
     total = len(list_transaksi)
-    return render_template('dashboard/dashboard_selesai.html', list_transaksi = list_transaksi, total=total)
+    return render_template('dashboard/dashboard_selesai.html', list_transaksi = list_transaksi, total=total , count1=count1, count2=count2, count3=count3, count4=count4)
 
 @dashboard.route('/batal')
 @login_required
 def batal():
+    count1 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Antrean').count()
+    count2 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dalam Proses').count()
+    count3 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Selesai').count()
+    count4 = Transaksi_Penjemputan.query.filter_by(status_transaksi='Dibatalkan').count()
     list_transaksi = Transaksi_Penjemputan.query.filter_by(status_transaksi = 'Dibatalkan').all()
     total = len(list_transaksi)
-    return render_template('dashboard/dashboard_pembatalan.html', list_transaksi = list_transaksi, total=total)
+    return render_template('dashboard/dashboard_pembatalan.html', list_transaksi = list_transaksi, total=total , count1=count1, count2=count2, count3=count3, count4=count4)
+
+@dashboard.route('/membatalkan/<int:id>')
+@login_required
+def membatalkan(id):
+    transaksi = Transaksi_Penjemputan.query.filter_by(id=id).first()
+    
+    transaksi.status_transaksi = 'Dibatalkan'
+    db.session.commit()
+    flash('Transaksi berhasil dibatalkan', 'warning')
+    return redirect(url_for('dashboard.dalam_proses'))
+
+@dashboard.route('/membatal/<int:id>')
+@login_required
+def membatal(id):
+    transaksi = Transaksi_Penjemputan.query.filter_by(id=id).first()
+    
+    transaksi.status_transaksi = 'Dibatalkan'
+    db.session.commit()
+    flash('Transaksi berhasil dibatalkan', 'warning')
+    return redirect(url_for('dashboard.index'))
